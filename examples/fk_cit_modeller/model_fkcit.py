9f1723b4fefc91303d649b51a22a142ad7e8d0bb
graik <graik@users.noreply.github.com>
2017-07-08 22:53:57 +0200

    modeller example

---
+++ examples/fk_cit_modeller/model_fkcit.py
@@ -0,0 +1,44 @@
+import modeller as M
+import modeller.automodel as AM
+import time
+
+# request verbose output
+M.log.verbose() 
+
+# create a new MODELLER environment to build this model in
+env = M.environ()
+
+# directories for input atom files
+env.io.atom_files_directory = ['.']
+
+class CustomModel( AM.automodel ):
+
+    def special_restraints(self, aln):
+        """
+        Fix input domains as rigid bodies -- does not seem to have much
+        effect on speed though
+        """
+        
+        # fix domains as rigid bodies to save time (manual p. 58)
+        rb = M.rigid_body( self.residue_range( '4', '110' ) )
+        self.restraints.rigid_bodies.append( rb )
+
+        rb = M.rigid_body( self.residue_range( '136', '361') )
+        self.restraints.rigid_bodies.append( rb )
+
+
+a = CustomModel(env,
+                alnfile='target_aln.pir',
+                knowns= ('01_fk', '02_cit'),
+                sequence = 'target')
+
+a.starting_model = 1
+a.ending_model = 1
+
+## a.very_fast() ## does not seem to work for us
+
+clock_start = time.time()
+a.make()
+
+clock_end = time.time()
+print "Run finished after: ", clock_end - clock_start, " seconds."
---
+++ examples/fk_cit_modeller/prepare4modeller.py
@@ -0,0 +1,33 @@
+import Biskit as B
+import Biskit.Mod.modUtils as U
+
+fk = B.PDBModel('01_fk.pdb')
+cit= B.PDBModel('02_cit.pdb')
+
+seq_fk = fk.sequence()
+seq_linker = 'TG' + 10*'GS' + 'TG'
+seq_cit = cit.sequence()
+
+
+seq_target = 'MTG' + seq_fk + seq_linker + seq_cit + '*'
+aln_fk     = '---' + seq_fk + '-'*(len(seq_linker)+len(seq_cit)) + '*'
+aln_cit    = '---' + '-'*(len(seq_fk)+len(seq_linker)) + seq_cit + '*'
+
+def seq2pir( seq, name,
+                 alntype='structure',
+                 ):
+    """
+    format sequence as PIR alignment entry
+    see: https://salilab.org/modeller/9v7/manual/node445.html
+    """
+    r = '>P1;%s\n' % name
+    r += '%s:%s:.:.:.:.::::\n' % (alntype, name)
+    r += U.format_fasta(seq, width=75)
+    
+    return r
+
+with open('target_aln.pir', 'w') as f:
+    f.write( seq2pir( seq_target, 'target', alntype='sequence') + '\n\n' )
+    f.write( seq2pir( aln_fk, '01_fk' ) + '\n\n' )
+    f.write( seq2pir( aln_cit, '02_cit')+ '\n\n' )
+
